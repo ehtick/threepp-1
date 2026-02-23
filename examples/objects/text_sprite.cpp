c46616a244200d286fd0db707f0589bee7790a1f
Lars Ivar Hatledal <laht@ntnu.no>
2026-02-23 11:40:39 +0100

    add win32 TextSprite example

---
+++ examples/objects/text_sprite.cpp
@@ -0,0 +1,30 @@
+
+#include "threepp/objects/TextSprite.hpp"
+#include "threepp/threepp.hpp"
+
+using namespace threepp;
+
+int main() {
+
+    Canvas canvas("TextNode");
+    GLRenderer renderer(canvas.size());
+
+    Scene scene;
+    PerspectiveCamera camera(75, canvas.aspect(), 0.1f, 1000);
+    camera.position.z = 5;
+
+    auto node = TextSprite::create("C:/Windows/Fonts/Arial.ttf");
+    node->setText("Hello world");
+    node->position.set(0, 10, 0);
+    scene.add(node);
+
+    auto grid = GridHelper::create(20, 10);
+    scene.add(grid);
+
+    OrbitControls controls(camera, canvas);
+
+    canvas.animate([&] {
+        renderer.render(scene, camera);
+    });
+
+}
